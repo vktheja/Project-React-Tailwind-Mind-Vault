26e14be6d887d277a237884575e7e84c78a00dfc
vktheja <vkthejal@gmail.com>
2026-01-08 17:09:05 -0500

    Note and notes styling almost done

--- src/components/Notes/Note.jsx
+++ src/components/Notes/Note.jsx
@@ -5,11 +5,11 @@ import Delete from "../../shared/icons/Delete";
 const Note = (props) => {
   //   const { note } = props;
   return (
-    <div className="flex flex-col max-w-3xl bg-white mx-4 mds:mx-auto my-6 px-4 rounded-md shadow-lg shadow-black/10 border border-gray-600/20">
-      <div className="border-b py-2 border-gray-200 text-slate-500 font-semibold text-lg">
+    <div className="flex flex-col bg-white px-4 rounded-md shadow-lg shadow-black/10 border border-gray-600/20">
+      <div className="border-b py-2 border-gray-200 text-slate-500 font-semibold text-base mds:text-sm">
         First Note
       </div>
-      <div className="py-2 h-30 overflow-y-auto">
+      <div className="py-2 h-32 overflow-y-auto text-base mds:text-sm">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium
         soluta cumque nihil iure quibusdam, dolores nostrum asperiores deserunt
         omnis magni sapiente ullam doloribus quis. Similique pariatur tempora
@@ -18,18 +18,18 @@ const Note = (props) => {
       <div className="border-b border-gray-200 mt-2 "></div>
       <div className="flex items-center justify-between py-2">
         <div className="flex items-center space-x-1 text-sm text-gray-600">
-          <Date className="w-4" />
+          <Date className="w-4 mds:w-3" />
           <p>January 08, 2026</p>
         </div>
         <div className="flex gap-6 items-center">
-          <div className="flex items-center space-x-1 text-sm text-gray-600">
-            <Edit className="w-4" />
-            <p>Edit</p>
-          </div>
-          <div className="flex items-center space-x-1 text-sm text-gray-600">
-            <Delete className="w-4" />
-            <p>Delete</p>
-          </div>
+          <button className="flex items-center space-x-1 text-sm mds:text-xs text-gray-600 hover:text-blue-500 cursor-pointer">
+            <Edit className="w-4 mds:w-3 transition-colors duration-200" />
+            <span>Edit</span>
+          </button>
+          <button className="flex items-center space-x-1 text-sm mds:text-xs text-gray-600 hover:text-red-500 cursor-pointer">
+            <Delete className="w-4 mds:w-3 transition-colors duration-200" />
+            <span>Delete</span>
+          </button>
         </div>
       </div>
     </div>
--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -2,7 +2,10 @@ import Note from "./Note";
 const Notes = (props) => {
   const { notes } = props;
   return (
-    <div>
+    <div className="grid grid-cols-1 mds:grid-cols-2 gap-4 auto-rows-fr max-w-3xl mx-4 mds:mx-auto">
+      <Note />
+      <Note />
+      <Note />
       <Note />
     </div>
   );
